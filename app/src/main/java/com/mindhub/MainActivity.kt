a9b1bb37da8254818dfca5d4cd5e3c143448e704
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-02 14:37:55 -0300

    feat: add navigator composable in main activity

--- app/src/main/java/com/mindhub/MainActivity.kt
+++ app/src/main/java/com/mindhub/MainActivity.kt
@@ -3,44 +3,17 @@ package com.mindhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mindhub.ui.theme.MindHubTheme
+import com.mindhub.view.NavGraphs
+import com.ramcosta.composedestinations.DestinationsNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MindHubTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MindHubTheme {
-        Greeting("Android")
     }
 }
