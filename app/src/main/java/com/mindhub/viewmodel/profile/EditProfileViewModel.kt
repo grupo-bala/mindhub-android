ebdba9b550342b2317abe173c4d190f9b7dab313
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-10-04 20:20:09 -0300

    feat: update edit profile view model with user info

--- app/src/main/java/com/mindhub/viewmodel/profile/EditProfileViewModel.kt
+++ app/src/main/java/com/mindhub/viewmodel/profile/EditProfileViewModel.kt
@@ -1,6 +1,7 @@
 package com.mindhub.viewmodel.profile
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
@@ -8,18 +9,18 @@ import com.mindhub.model.entities.Badge
 import com.mindhub.services.UserInfo
 
 class EditProfileViewModel() : ViewModel() {
-    var name by mutableStateOf(UserInfo.name)
-    var username by mutableStateOf(UserInfo.username)
-    var email by mutableStateOf(UserInfo.email)
-    var achievement by mutableStateOf(UserInfo.achievement)
-    var achievements = mutableListOf<Badge>()
+    var name by mutableStateOf(UserInfo!!.name)
+    var username by mutableStateOf(UserInfo!!.username)
+    var email by mutableStateOf(UserInfo!!.email)
+    var badge by mutableStateOf(UserInfo!!.currentBadge)
+    var badges = mutableListOf<Badge>()
 
     private var isAchievementsLoaded = false
     fun loadAchievements() {
         if (!isAchievementsLoaded) {
             // TODO: load achievements based on the API
 
-            achievements.addAll(listOf(Badge("A"), Badge("B"), Badge("C")))
+            badges.addAll(listOf(Badge("A"), Badge("B"), Badge("C")))
 
             isAchievementsLoaded = true
         }
