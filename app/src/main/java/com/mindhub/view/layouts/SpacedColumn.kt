0e926b79352aab3345c4cfcb60e932fa9d28f5cd
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-04 13:22:08 -0300

    refactor: update spaced column parameters

--- app/src/main/java/com/mindhub/view/layouts/SpacedColumn.kt
+++ app/src/main/java/com/mindhub/view/layouts/SpacedColumn.kt
@@ -3,7 +3,6 @@ package com.mindhub.view.layouts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,13 +11,15 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun SpacedColumn(
     spacing: Int,
+    verticalAlignment: Alignment.Vertical,
+    horizontalAlignment: Alignment.Horizontal,
     modifier: Modifier = Modifier,
     content: @Composable (ColumnScope.() -> Unit)
 ) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(spacing.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(spacing.dp, verticalAlignment),
+        horizontalAlignment = horizontalAlignment,
+        modifier = modifier,
         content = content
     )
 }
