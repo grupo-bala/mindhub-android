ab13a902b7027e1104bd34df3818ee372e7fc135
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-04 13:22:53 -0300

    feat: init create events page

---
+++ app/src/main/java/com/mindhub/view/events/CreateEventInfo.kt
@@ -0,0 +1,225 @@
+package com.mindhub.view.events
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.mindhub.services.toBrazilianDateFormat
+import com.mindhub.services.toTimeFormat
+import com.mindhub.ui.theme.MindHubTheme
+import com.mindhub.view.layouts.SpacedColumn
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Destination
+@Composable
+fun CreateEventInfo(
+    navigator: DestinationsNavigator
+) {
+    val focusManager = LocalFocusManager.current
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+        ) {
+            TopAppBar(
+                title = { Text(text = "Adicionar um evento", style = MaterialTheme.typography.titleMedium) },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Close, contentDescription = null)
+                    }
+                },
+                actions = {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Continuar")
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            SpacedColumn(
+                spacing = 8,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalAlignment = Alignment.Top,
+                modifier = Modifier
+                    .weight(1.0f)
+                    .padding(horizontal = 16.dp)
+                    .padding(bottom = 16.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                OutlinedTextField(
+                    value = "",
+                    label = { Text(text = "Título") },
+                    placeholder = { Text(text = "Digite o título") },
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    label = { Text(text = "Descrição") },
+                    placeholder = { Text(text = "Digite a descrição") },
+                    onValueChange = {},
+                    modifier = Modifier
+                        .height(300.dp)
+                        .fillMaxWidth()
+                )
+
+                var isDatePickerOpened by remember { mutableStateOf(false) }
+                var selectedDate by remember { mutableStateOf("") }
+                val datePickerState = rememberDatePickerState()
+
+                if (isDatePickerOpened) {
+                    DatePickerDialog(
+                        onDismissRequest = { isDatePickerOpened = false },
+                        confirmButton = {
+                            Button(onClick = {
+                                datePickerState.selectedDateMillis?.let {
+                                    selectedDate = it.toBrazilianDateFormat()
+                                }
+
+                                isDatePickerOpened = false
+                            }) {
+                                Text(text = "Escolher data")
+                            }
+                        }
+                    ) {
+                        DatePicker(state = datePickerState)
+                    }
+                }
+
+                val dateInteraction = remember { MutableInteractionSource() }.also {
+                    if (it.collectIsFocusedAsState().value) {
+                        isDatePickerOpened = true
+                        focusManager.clearFocus(force = true)
+                    }
+                }
+
+                OutlinedTextField(
+                    value = selectedDate,
+                    onValueChange = { },
+                    label = { Text(text = "Data") },
+                    placeholder = { Text(text = "Selecione a data") },
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    readOnly = true,
+                    interactionSource = dateInteraction
+                )
+
+                var isTimePickerOpened by remember { mutableStateOf(false) }
+                var selectedTime by remember { mutableStateOf("") }
+                val timePickerState = rememberTimePickerState()
+
+                if (isTimePickerOpened) {
+                    Dialog(onDismissRequest = { isTimePickerOpened = false }) {
+                        Surface(
+                            modifier = Modifier
+                                .wrapContentWidth()
+                                .wrapContentHeight(),
+                            shape = MaterialTheme.shapes.large,
+                            tonalElevation = AlertDialogDefaults.TonalElevation
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                TimePicker(
+                                    state = timePickerState,
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                                
+                                Row(
+                                    horizontalArrangement = Arrangement.End,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(8.dp)
+                                ) {
+                                    Button(onClick = {
+                                        selectedTime = timePickerState.toTimeFormat()
+                                        isTimePickerOpened = false
+                                    }) {
+                                        Text(text = "Escolher horário")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                val timeInteraction = remember { MutableInteractionSource() }.also {
+                    if (it.collectIsFocusedAsState().value) {
+                        isTimePickerOpened = true
+                        focusManager.clearFocus(force = true)
+                    }
+                }
+
+                OutlinedTextField(
+                    value = selectedTime,
+                    onValueChange = { },
+                    label = { Text(text = "Horário") },
+                    placeholder = { Text(text = "Selecione o horário") },
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    readOnly = true,
+                    interactionSource = timeInteraction
+                )
+
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CreateEventInfoPreview() {
+    MindHubTheme {
+        CreateEventInfo(navigator = EmptyDestinationsNavigator)
+    }
+}
---
+++ app/src/main/java/com/mindhub/view/events/CreateEventLocation.kt
@@ -0,0 +1,141 @@
+package com.mindhub.view.events
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.mindhub.ui.theme.MindHubTheme
+import com.mindhub.view.layouts.SpacedColumn
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+
+@SuppressLint("MissingPermission")
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
+@Destination
+@Composable
+fun CreateEventLocation(
+    navigator: DestinationsNavigator
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+        ) {
+            TopAppBar(
+                title = { Text(text = "Adicionar um evento", style = MaterialTheme.typography.titleMedium) },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Close, contentDescription = null)
+                    }
+                },
+                actions = {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Adicionar")
+                    }
+                }
+            )
+
+            SpacedColumn(
+                spacing = 8,
+                verticalAlignment = Alignment.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .weight(1.0f)
+            ) {
+                val locationPermissionState = rememberPermissionState(
+                    permission = android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+
+                LaunchedEffect(key1 = true) {
+                    locationPermissionState.launchPermissionRequest()
+                }
+
+                var eventPosition by remember { mutableStateOf(LatLng(0.0, 0.0)) }
+
+                if (locationPermissionState.status.isGranted) {
+                    val context = LocalContext.current
+                    val locationService = LocationServices.getFusedLocationProviderClient(context)
+                    val cameraPositionState = rememberCameraPositionState {
+                        position = CameraPosition.fromLatLngZoom(LatLng(0.0, 0.0), 10.0f)
+                    }
+
+                    LaunchedEffect(key1 = true) {
+                        locationService.lastLocation.addOnSuccessListener {
+                            cameraPositionState.position = CameraPosition.fromLatLngZoom(
+                                LatLng(it.latitude, it.longitude),
+                                15.0f
+                            )
+                        }
+                    }
+
+                    GoogleMap(
+                        cameraPositionState = cameraPositionState,
+                        onMapClick = { eventPosition = it },
+                        properties = MapProperties(isMyLocationEnabled = true),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1.0f)
+                    ) {
+                        Marker(
+                            state = MarkerState(eventPosition)
+                        )
+                    }
+                } else {
+                    GoogleMap(
+                        onMapClick = { eventPosition = it },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1.0f)
+                    ) {
+                        Marker(
+                            state = MarkerState(eventPosition)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun CreateEventLocationPreview() {
+    MindHubTheme {
+        CreateEventLocation(navigator = EmptyDestinationsNavigator)
+    }
+}
