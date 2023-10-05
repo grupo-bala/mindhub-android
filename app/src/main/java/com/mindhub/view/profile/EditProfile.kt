b213e5731f8bc09fd6d4fabaeecdf0b7a253b9c2
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-10-05 03:45:21 -0300

    feat: update edit profile fields

--- app/src/main/java/com/mindhub/view/profile/EditProfile.kt
+++ app/src/main/java/com/mindhub/view/profile/EditProfile.kt
@@ -1,26 +1,37 @@
 package com.mindhub.view.profile
 
+import android.graphics.Paint.Align
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,6 +43,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -42,33 +55,48 @@ import com.mindhub.model.entities.Expertise
 import com.mindhub.model.entities.User
 import com.mindhub.services.UserInfo
 import com.mindhub.ui.theme.MindHubTheme
+import com.mindhub.view.composables.Suspended
 import com.mindhub.view.layouts.SpacedColumn
 import com.mindhub.viewmodel.profile.EditProfileViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Destination
 @Composable
 fun EditProfile(
     navigator: DestinationsNavigator,
 ) {
     val viewModel: EditProfileViewModel = viewModel()
-    var expanded by remember {
+    var hasFailure by remember {
         mutableStateOf(false)
     }
-    val interaction = remember { MutableInteractionSource() }.also {
+    var isMenuExpanded by remember {
+        mutableStateOf(false)
+    }
+    var isExpertisesMenuExpanded by remember {
+        mutableStateOf(false)
+    }
+
+    val badgesInteraction = remember { MutableInteractionSource() }.also {
         if (it.collectIsPressedAsState().value) {
-            expanded = true
+            isMenuExpanded = true
+        }
+    }
+    val expertisesInteraction = remember { MutableInteractionSource() }.also {
+        if (it.collectIsPressedAsState().value) {
+            isExpertisesMenuExpanded = true
         }
     }
 
-    viewModel.loadAchievements()
+    viewModel.loadExpertises()
+    viewModel.loadBadges()
 
     Surface(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         SpacedColumn(
             spacing = 8,
@@ -103,16 +131,6 @@ fun EditProfile(
             Spacer(modifier = Modifier.height(8.dp))
 
             OutlinedTextField(
-                value = viewModel.username,
-                label = { Text(text = "Nome de usuário") },
-                placeholder = { Text(text = "Digite o seu nome de usuário") },
-                onValueChange = { viewModel.username = it },
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            OutlinedTextField(
                 value = viewModel.email,
                 label = { Text(text = "Email") },
                 placeholder = { Text(text = "Digite o seu email") },
@@ -132,29 +150,109 @@ fun EditProfile(
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     OutlinedTextField(
-                        value = viewModel.badge.title,
+                        value = viewModel.badge,
                         readOnly = true,
                         label = { Text(text = "Conquista") },
                         onValueChange = {},
                         trailingIcon = { Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null) },
-                        interactionSource = interaction,
+                        interactionSource = badgesInteraction,
                         modifier = Modifier.fillMaxWidth()
                     )
                 }
 
-                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                DropdownMenu(expanded = isMenuExpanded, onDismissRequest = { isMenuExpanded = false }) {
                     for (badge in viewModel.badges) {
-                        DropdownMenuItem(text = { Text(badge.title) }, onClick = { viewModel.badge = badge })
+                        DropdownMenuItem(text = { Text(badge) }, onClick = {
+                            viewModel.badge = badge
+                            isMenuExpanded = false
+                        })
                     }
                 }
             }
 
-            Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = viewModel.selectedExpertises.fold("") { acc, curr ->
+                    if (acc.isEmpty()) {
+                        curr.title
+                    } else {
+                        "$acc, ${curr.title}"
+                    }
+                },
+                readOnly = true,
+                label = { Text(text = "Expertises") },
+                onValueChange = {},
+                trailingIcon = { Icon(imageVector = Icons.Default.Create, contentDescription = null) },
+                interactionSource = expertisesInteraction,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            if (isExpertisesMenuExpanded) {
+                ModalBottomSheet(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .defaultMinSize(minHeight = 254.dp),
+                    onDismissRequest = { isExpertisesMenuExpanded = false }
+                ) {
+                    Suspended(
+                        isLoading = viewModel.expertises.size < 1,
+                        loadingSize = 64.dp,
+                        strokeWidth = 4.dp,
+                        color = MaterialTheme.colorScheme.onBackground
+                    ) {
+                        FlowRow(
+                            horizontalArrangement = Arrangement
+                                .spacedBy(
+                                    8.dp,
+                                    alignment = Alignment.CenterHorizontally
+                                ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(
+                                    horizontal = 32.dp,
+                                    vertical = 32.dp
+                                ),
+                        ) {
+                            viewModel.expertises.forEach {
+                                InputChip(
+                                    selected = viewModel.isSelected(it.title),
+                                    onClick = { viewModel.toggleExpertise(it) },
+                                    label = { Text(text = it.title) },
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!hasFailure) {
+                Spacer(modifier = Modifier.height(32.dp))
+            } else {
+                Text(
+                    text = "Algo de errado aconteceu!",
+                    color = MaterialTheme.colorScheme.error,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+            }
 
             OutlinedButton(
                 onClick = {
-                    // TODO: call the API to confirm the edit
-                    navigator.popBackStack()
+                    viewModel.update(
+                        onSuccess = {
+                            UserInfo!!.email = viewModel.email
+                            UserInfo!!.name = viewModel.name
+                            UserInfo!!.currentBadge = viewModel.badge
+                            UserInfo!!.expertises = viewModel.selectedExpertises
+
+                            navigator.popBackStack()
+                        },
+                        onFailure = {
+                            hasFailure = true
+                        }
+                    )
                 },
                 modifier = Modifier.fillMaxWidth()
             ) {
@@ -179,7 +277,7 @@ fun EditProfilePreview() {
         username = "username",
         email = "user@gmail.com",
         xp = 727,
-        currentBadge = Badge("Aprendiz"),
+        currentBadge ="Aprendiz",
         expertises = listOf(Expertise("Matemática"), Expertise("Geografia"), Expertise("Química")),
         token = ""
     )
