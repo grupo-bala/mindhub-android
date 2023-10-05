f97f07785cd889800107d5a13928c757721e929e
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-10-05 03:43:23 -0300

    feat: add update method in edit profile view model

--- app/src/main/java/com/mindhub/viewmodel/profile/EditProfileViewModel.kt
+++ app/src/main/java/com/mindhub/viewmodel/profile/EditProfileViewModel.kt
@@ -1,28 +1,88 @@
 package com.mindhub.viewmodel.profile
 
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mindhub.model.api.ExpertiseApi
+import com.mindhub.model.api.UpdateRequest
+import com.mindhub.model.api.UserApi
 import com.mindhub.model.entities.Badge
+import com.mindhub.model.entities.Expertise
 import com.mindhub.services.UserInfo
+import kotlinx.coroutines.launch
 
 class EditProfileViewModel() : ViewModel() {
     var name by mutableStateOf(UserInfo!!.name)
-    var username by mutableStateOf(UserInfo!!.username)
     var email by mutableStateOf(UserInfo!!.email)
+    var selectedExpertises = mutableStateListOf<Expertise>()
     var badge by mutableStateOf(UserInfo!!.currentBadge)
-    var badges = mutableListOf<Badge>()
+    var badges = mutableStateListOf<String>()
+    var isLoading by mutableStateOf(false)
 
-    private var isAchievementsLoaded = false
-    fun loadAchievements() {
-        if (!isAchievementsLoaded) {
+    var expertises = mutableStateListOf<Expertise>()
+
+    private var isBadgesLoaded = false
+    private var isExpertisesLoaded = false
+
+    fun loadBadges() {
+        if (!isBadgesLoaded) {
             // TODO: load achievements based on the API
 
-            badges.addAll(listOf(Badge("A"), Badge("B"), Badge("C")))
+            badges.addAll(listOf("Aprendiz", "Experiente", "Mestre"))
 
-            isAchievementsLoaded = true
+            isBadgesLoaded = true
+        }
+    }
+
+    fun isSelected(expertise: String): Boolean {
+        return selectedExpertises.contains(Expertise(expertise))
+    }
+
+    fun toggleExpertise(expertise: Expertise) {
+        val valueInList = selectedExpertises.find { it == expertise }
+
+        if (valueInList == null && selectedExpertises.size < 3) {
+            selectedExpertises.add(expertise)
+        } else if (valueInList != null && selectedExpertises.size > 1) {
+            selectedExpertises.remove(expertise)
+        }
+    }
+
+    fun loadExpertises() {
+        if (!isExpertisesLoaded) {
+            for (expertise in UserInfo!!.expertises) {
+                selectedExpertises.add(expertise)
+            }
+
+            viewModelScope.launch {
+                try {
+                    expertises.addAll(ExpertiseApi.getAllExpertises())
+                } catch (_: Exception) { }
+            }
+
+            isExpertisesLoaded = true
+        }
+    }
+
+    fun update(
+        onSuccess: () -> Unit,
+        onFailure: (String?) -> Unit
+    ) {
+        viewModelScope.launch {
+            try {
+                isLoading = true
+
+                val res = UserApi.update(UpdateRequest(name, email, selectedExpertises))
+
+                onSuccess()
+            } catch (e: Exception) {
+                onFailure(e.message)
+            }
+
+            isLoading = false
         }
     }
 }
