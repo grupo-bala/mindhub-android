1ee3e1ecbcf128197c552c06aa3510dd988aacd9
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-10-10 18:49:16 -0300

    feat: remove expertise and badge dependencies from edit profile view model

--- app/src/main/java/com/mindhub/viewmodel/profile/EditProfileViewModel.kt
+++ app/src/main/java/com/mindhub/viewmodel/profile/EditProfileViewModel.kt
@@ -8,7 +8,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mindhub.model.api.ExpertiseApi
 import com.mindhub.model.api.UpdateRequest
+import com.mindhub.model.api.UserApi
 import com.mindhub.model.api.UserFakeApi
+import com.mindhub.model.entities.Badge
 import com.mindhub.model.entities.Expertise
 import com.mindhub.services.UserInfo
 import kotlinx.coroutines.launch
@@ -16,57 +18,11 @@ import kotlinx.coroutines.launch
 class EditProfileViewModel() : ViewModel() {
     var name by mutableStateOf(UserInfo!!.name)
     var email by mutableStateOf(UserInfo!!.email)
-    var selectedExpertises = mutableStateListOf<Expertise>()
-    var badge by mutableStateOf(UserInfo!!.currentBadge)
-    var badges = mutableStateListOf<String>()
     var isLoading by mutableStateOf(false)
 
-    var expertises = mutableStateListOf<Expertise>()
-
-    private var isBadgesLoaded = false
-    private var isExpertisesLoaded = false
-
-    fun loadBadges() {
-        if (!isBadgesLoaded) {
-            // TODO: load achievements based on the API
-
-            badges.addAll(listOf("Aprendiz", "Experiente", "Mestre"))
-
-            isBadgesLoaded = true
-        }
-    }
-
-    fun isSelected(expertise: String): Boolean {
-        return selectedExpertises.contains(Expertise(expertise))
-    }
-
-    fun toggleExpertise(expertise: Expertise) {
-        val valueInList = selectedExpertises.find { it == expertise }
-
-        if (valueInList == null && selectedExpertises.size < 3) {
-            selectedExpertises.add(expertise)
-        } else if (valueInList != null && selectedExpertises.size > 1) {
-            selectedExpertises.remove(expertise)
-        }
-    }
-
-    fun loadExpertises() {
-        if (!isExpertisesLoaded) {
-            for (expertise in UserInfo!!.expertises) {
-                selectedExpertises.add(expertise)
-            }
-
-            viewModelScope.launch {
-                try {
-                    expertises.addAll(ExpertiseApi.getAllExpertises())
-                } catch (_: Exception) { }
-            }
-
-            isExpertisesLoaded = true
-        }
-    }
-
     fun update(
+        selectedBadge: Badge,
+        selectedExpertises: List<Expertise>,
         onSuccess: () -> Unit,
         onFailure: (String?) -> Unit
     ) {
@@ -74,7 +30,7 @@ class EditProfileViewModel() : ViewModel() {
             try {
                 isLoading = true
 
-                val res = UserFakeApi.update(UpdateRequest(name, email, selectedExpertises))
+                UserFakeApi.update(UpdateRequest(name, email, selectedExpertises, selectedBadge))
 
                 onSuccess()
             } catch (e: Exception) {
