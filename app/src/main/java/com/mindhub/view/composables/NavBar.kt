b6b267e0109763b67e15c9bcd292dffba4c1953a
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-12 01:03:51 -0300

    fix: update nav bar height

--- app/src/main/java/com/mindhub/view/composables/NavBar.kt
+++ app/src/main/java/com/mindhub/view/composables/NavBar.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mindhub.ui.theme.MindHubTheme
 import com.mindhub.view.layouts.AppScaffold
 import com.mindhub.view.layouts.Views
@@ -21,13 +24,14 @@ fun NavBar(
     currentView: Views
 ) {
     BottomAppBar(
-        containerColor = MaterialTheme.colorScheme.primary
+        containerColor = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.height(64.dp)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceAround,
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
         ) {
             for (view in Views.entries) {
                 Icon(
