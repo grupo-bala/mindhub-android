0b1e78348f29cefc5dfd47df0c416dab820e98df
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-10-18 11:23:48 -0300

    refactor: refactor post vm interface making implicit the get and set through post proterties

--- app/src/main/java/com/mindhub/viewmodel/post/PostViewModelInterface.kt
+++ app/src/main/java/com/mindhub/viewmodel/post/PostViewModelInterface.kt
@@ -3,10 +3,7 @@ package com.mindhub.viewmodel.post
 import com.mindhub.model.entities.Expertise
 
 interface PostViewModelInterface {
-    fun setTitle(title: String)
-    fun getTitle(): String
-    fun setContent(content: String)
-    fun getContent(): String
-    fun setExpertise(expertise: Expertise)
-    fun getExpertise(): String
+    var title: String
+    var content: String
+    var expertise: Expertise
 }
