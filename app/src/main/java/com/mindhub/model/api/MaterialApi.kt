270052ed3de1d6d36aaf132e7b393c49c557c0c2
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-10-18 11:25:16 -0300

    fead: add update method in provider interface and fake api

--- app/src/main/java/com/mindhub/model/api/MaterialApi.kt
+++ app/src/main/java/com/mindhub/model/api/MaterialApi.kt
@@ -3,8 +3,10 @@ package com.mindhub.model.api
 import com.mindhub.model.entities.Expertise
 import com.mindhub.model.entities.Material
 import com.mindhub.model.entities.User
+import kotlinx.serialization.Serializable
 
-data class MaterialCreateRequest(
+@Serializable
+data class MaterialRequest(
     val user: User,
     val title: String,
     val content: String,
@@ -12,7 +14,8 @@ data class MaterialCreateRequest(
 )
 
 interface MaterialProvider {
-    suspend fun create(material: MaterialCreateRequest)
+    suspend fun create(material: MaterialRequest)
+    suspend fun update(materialId: Int, materialUpdated: MaterialRequest)
     suspend fun getOne(id: Int): Material
     suspend fun get(title: String): List<Material>
 }
@@ -21,7 +24,7 @@ object MaterialFakeApi: MaterialProvider {
     private val materials = mutableListOf<Material>()
     private var count: Int = 0
 
-    override suspend fun create(material: MaterialCreateRequest) {
+    override suspend fun create(material: MaterialRequest) {
         count += 1
         materials.add(Material(
             id = count,
@@ -30,6 +33,14 @@ object MaterialFakeApi: MaterialProvider {
             expertise = material.expertise,
             user = material.user
         ))
+    }
+
+    override suspend fun update(materialId: Int, materialUpdated: MaterialRequest) {
+        val material = materials.find { it.id == materialId } ?: throw Exception()
+
+        material.content = materialUpdated.content
+        material.title = materialUpdated.title
+        material.expertise = materialUpdated.expertise
     }
 
     override suspend fun getOne(id: Int): Material {
