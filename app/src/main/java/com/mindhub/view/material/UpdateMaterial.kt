40d788ce978540aa15157b4579248534ff7a5002
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-10-19 09:44:53 -0300

    feat: add remove button and logic in update material view

--- app/src/main/java/com/mindhub/view/material/UpdateMaterial.kt
+++ app/src/main/java/com/mindhub/view/material/UpdateMaterial.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mindhub.model.entities.Expertise
@@ -77,9 +80,29 @@ fun UpdateMaterial(
             SpacedColumn(
                 spacing = 16,
                 verticalAlignment = Alignment.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 PostInputs(viewModel = materialViewModel)
+
+                OutlinedButton(
+                    onClick = { materialViewModel.remove(
+                        onSuccess = {
+                            navigator.popBackStack()
+                        },
+                        onFailure = {
+                            feedbackError = it
+                        }
+                    ) },
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colorScheme.error),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {
+                    Text(
+                        text = "Remover material",
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
 
                 if (feedbackError != null) {
                     Text(
