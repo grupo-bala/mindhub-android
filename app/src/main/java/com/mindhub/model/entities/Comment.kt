d3512c40ccf2c05ea9be65889dd23674b377c519
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-21 16:56:40 -0300

    feat: add comment entity

---
+++ app/src/main/java/com/mindhub/model/entities/Comment.kt
@@ -0,0 +1,15 @@
+package com.mindhub.model.entities
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Comment(
+    val id: Int,
+    val postId: Int,
+    val username: String,
+    var content: String,
+    var isBestAnswer: Boolean,
+    var score: Int,
+    var userScore: Int,
+    var replies: List<Comment>,
+)
