4f46321cc88072247c1aac55fe57e9281eaf5dff
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-22 16:15:23 -0300

    feat: add generic post view

---
+++ app/src/main/java/com/mindhub/view/post/PostView.kt
@@ -0,0 +1,103 @@
+package com.mindhub.view.post
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mindhub.model.api.AskFakeApi
+import com.mindhub.model.entities.Ask
+import com.mindhub.model.entities.Badge
+import com.mindhub.model.entities.Expertise
+import com.mindhub.model.entities.User
+import com.mindhub.ui.theme.MindHubTheme
+import com.mindhub.view.composables.PostInfo
+import com.mindhub.view.composables.Suspended
+import com.mindhub.view.composables.comment.CommentsView
+import com.mindhub.view.layouts.AppScaffold
+import com.mindhub.view.layouts.SpacedColumn
+import com.mindhub.view.layouts.Views
+import com.mindhub.viewmodel.ask.GetAskViewModel
+import com.mindhub.viewmodel.post.GetPostViewModel
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+
+@Composable
+fun PostView(
+    postId: Int,
+    viewModel: GetPostViewModel,
+    navigator: DestinationsNavigator
+) {
+
+    LaunchedEffect(key1 = true) {
+        viewModel.get(postId)
+    }
+
+    AppScaffold(
+        currentView = Views.ASK,
+        navigator = navigator,
+        hasBackArrow = true,
+        bottomAppBar = {}
+    ) {
+        Suspended(
+            isLoading = viewModel.isLoading
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                SpacedColumn(
+                    spacing = 8,
+                    verticalAlignment = Alignment.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.verticalScroll(rememberScrollState())
+                ) {
+                    if (viewModel.post == null) {
+                        Text(text = viewModel.feedback)
+                    } else {
+                        PostInfo(post = viewModel.post!!)
+                        CommentsView(postId = postId)
+                    }
+                }
+
+                if (viewModel.post != null) {
+                    FloatingActionButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.BottomEnd)
+                    ) {
+                        Icon(imageVector = Icons.Outlined.Email, contentDescription = null)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PostViewPreview() {
+    val viewModel: GetAskViewModel = viewModel()
+    val user = User("Teste", "teste", "teste87", 0, Badge(""), listOf(), "")
+    AskFakeApi.asks.add(Ask(0, "Como aplicar o teorema de pitágoras em um círculo?", "teste", 76, user, Expertise("Matemática")));
+
+    MindHubTheme {
+        PostView(
+            navigator = EmptyDestinationsNavigator,
+            postId = 0,
+            viewModel = viewModel
+        )
+    }
+}
