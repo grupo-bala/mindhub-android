b519677ea322dd77a749774ba48870592bf8e41c
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-10-21 22:09:31 -0300

    feat: add generics methods to post viewmodel interface

--- app/src/main/java/com/mindhub/viewmodel/post/PostViewModelInterface.kt
+++ app/src/main/java/com/mindhub/viewmodel/post/PostViewModelInterface.kt
@@ -6,4 +6,20 @@ interface PostViewModelInterface {
     var title: String
     var content: String
     var expertise: Expertise
+
+    fun create(
+        onSuccess: () -> Unit,
+        onFailure: (String?) -> Unit,
+    )
+    fun update(
+        postId: Int,
+        onSuccess: () -> Unit,
+        onFailure: (String?) -> Unit,
+    )
+    fun remove(
+        postId: Int,
+        onSuccess: () -> Unit,
+        onFailure: (String?) -> Unit
+    )
+    fun getType(): String
 }
