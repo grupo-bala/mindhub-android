8530c9665b199a329c3a9b3a382bcbea276f9ad5
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-22 16:12:56 -0300

    feat: add ask view

---
+++ app/src/main/java/com/mindhub/view/ask/AskView.kt
@@ -0,0 +1,23 @@
+package com.mindhub.view.ask
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mindhub.view.post.PostView
+import com.mindhub.viewmodel.ask.GetAskViewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Destination
+@Composable
+fun AskView(
+    askId: Int,
+    navigator: DestinationsNavigator
+) {
+    val getViewModel: GetAskViewModel = viewModel()
+
+    PostView(
+        postId = askId,
+        viewModel = getViewModel,
+        navigator = navigator
+    )
+}
