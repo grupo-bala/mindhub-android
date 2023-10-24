9b7578e687aea36b25d5a2a45ed3d9f6261362b7
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-10-24 11:17:41 -0300

    fix: fix material view model instantiate

--- app/src/main/java/com/mindhub/view/material/CreateMaterial.kt
+++ app/src/main/java/com/mindhub/view/material/CreateMaterial.kt
@@ -1,27 +1,9 @@
 package com.mindhub.view.material
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mindhub.ui.theme.MindHubTheme
 import com.mindhub.view.composables.PostCreate
-import com.mindhub.view.composables.PostInputs
 import com.mindhub.viewmodel.material.MaterialViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -30,9 +12,11 @@ import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 @Destination
 @Composable
 fun CreateMaterial(
-    navigator: DestinationsNavigator
+    navigator: DestinationsNavigator,
+    title: String? = null
 ) {
-    val viewModel = MaterialViewModel()
+    val viewModel: MaterialViewModel = viewModel()
+    viewModel.title = title ?: ""
 
     PostCreate(
         navigator = navigator,
