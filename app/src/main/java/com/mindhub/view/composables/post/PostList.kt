6b725c76428317eef0080101f07bd61cd0ad3284
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-24 14:40:33 -0300

    fix: add modifier in parameters

--- app/src/main/java/com/mindhub/view/composables/post/PostList.kt
+++ app/src/main/java/com/mindhub/view/composables/post/PostList.kt
@@ -3,14 +3,16 @@ package com.mindhub.view.composables.post
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.mindhub.model.entities.Post
 
 @Composable
 fun PostList(
     posts: List<Post>,
-    onClick: (Post) -> Unit
+    onClick: (Post) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    LazyColumn() {
+    LazyColumn(modifier = modifier) {
         items(posts) {
             PostItem(
                 title = it.title,
