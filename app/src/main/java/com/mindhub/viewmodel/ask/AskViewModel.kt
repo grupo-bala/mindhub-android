ad337293e9257f50591821d69aafbb0edc3ff6b5
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-10-24 11:13:50 -0300

    feat: add ask view model

---
+++ app/src/main/java/com/mindhub/viewmodel/ask/AskViewModel.kt
@@ -0,0 +1,84 @@
+package com.mindhub.viewmodel.ask
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mindhub.model.api.AskFakeApi
+import com.mindhub.model.entities.Ask
+import com.mindhub.model.entities.Expertise
+import com.mindhub.model.entities.Post
+import com.mindhub.services.UserInfo
+import com.mindhub.viewmodel.post.PostViewModelInterface
+import kotlinx.coroutines.launch
+
+class AskViewModel: ViewModel(), PostViewModelInterface {
+    override var title by mutableStateOf("")
+    override var content by mutableStateOf("")
+    override var expertise by mutableStateOf(Expertise(""))
+
+    override fun create(
+        onSuccess: (Post) -> Unit,
+        onFailure: (String?) -> Unit,
+    ) {
+        viewModelScope.launch {
+            try {
+                val post = AskFakeApi.create(Ask(
+                    id = -1,
+                    title = title,
+                    content = content,
+                    expertise = expertise,
+                    score = 0,
+                    user = UserInfo!!
+                ))
+
+                onSuccess(post)
+            } catch (e: Exception) {
+                onFailure(e.message)
+            }
+        }
+    }
+
+    override fun update(
+        postId: Int,
+        onSuccess: () -> Unit,
+        onFailure: (String?) -> Unit,
+    ) {
+        viewModelScope.launch {
+            try {
+                AskFakeApi.update(Ask(
+                    id = postId,
+                    title = title,
+                    content = content,
+                    expertise = expertise,
+                    user = UserInfo!!,
+                    score = 0
+                ))
+
+                onSuccess()
+            } catch (e: Exception) {
+                onFailure(e.message)
+            }
+        }
+    }
+
+    override fun remove(
+        postId: Int,
+        onSuccess: () -> Unit,
+        onFailure: (String?) -> Unit,
+    ) {
+        viewModelScope.launch {
+            try {
+                AskFakeApi.remove(postId)
+                onSuccess()
+            } catch (e: Exception) {
+                onFailure(e.message)
+            }
+        }
+    }
+
+    override fun getType(): String {
+        return "pergunta"
+    }
+}
