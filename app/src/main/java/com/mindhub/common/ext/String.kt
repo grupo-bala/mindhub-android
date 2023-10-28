c06a7fa2e49d9afce9d76c136780cc72da5f1aed
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-27 23:32:56 -0300

    feat: add string to unix date method

--- app/src/main/java/com/mindhub/common/ext/String.kt
+++ app/src/main/java/com/mindhub/common/ext/String.kt
@@ -1,9 +1,23 @@
 package com.mindhub.common.ext
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 fun String.ellipsis(maxSize: Int): String {
     if (maxSize >= this.length) {
         return this
     }
 
     return "${this.substring(0, maxSize - 1)}..."
+}
+
+fun String.dateToUnix(time: String): LocalDateTime {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    val date = LocalDateTime.parse(this, formatter)
+    val timeValues = time.split(":")
+
+    date.plusHours(timeValues[0].toLong())
+    date.plusMinutes(timeValues[1].toLong())
+
+    return date
 }
