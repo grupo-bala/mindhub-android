d376c2b625f626b138388561dd4f76ce59c821f9
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-27 23:35:30 -0300

    feat: add event fake api

---
+++ app/src/main/java/com/mindhub/model/api/EventApi.kt
@@ -0,0 +1,33 @@
+package com.mindhub.model.api
+
+import com.mindhub.model.entities.Badge
+import com.mindhub.model.entities.Event
+import com.mindhub.model.entities.User
+import java.time.LocalDateTime
+
+interface EventProvider {
+    suspend fun create(event: Event): Event
+    suspend fun get(id: Int): Event
+}
+
+object EventFakeApi : EventProvider {
+    private var count = 0
+
+    val events = mutableListOf<Event>().also {
+        val user = User("João", "joaum123@gmail.com", "jjaum", 0, Badge(""), listOf(), "")
+        it.add(Event(count++, user, "Teste 1", "teste", 2, LocalDateTime.now(), LocalDateTime.now(), 0.0, 0.0))
+        it.add(Event(count++, user, "Teste 2", "teste", 2, LocalDateTime.now(), LocalDateTime.now(), 0.0, 0.0))
+        it.add(Event(count++, user, "Teste 3", "teste", 2, LocalDateTime.now(), LocalDateTime.now(), 0.0, 0.0))
+    }
+    override suspend fun create(event: Event): Event {
+        events.add(
+            event.apply { this.id = count++ }
+        )
+
+        return event
+    }
+
+    override suspend fun get(id: Int): Event {
+        return events.find { it.id == id } ?: throw Exception()
+    }
+}
