1647dedbf92c0aaf811576f96fca10f875e5bbf9
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-06 20:36:08 -0300

    feat: add remove button to remove file selected

--- app/src/main/java/com/mindhub/view/composables/FileInput.kt
+++ app/src/main/java/com/mindhub/view/composables/FileInput.kt
@@ -4,16 +4,28 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,13 +35,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.mindhub.R
 import com.mindhub.ui.theme.MindHubTheme
+import com.mindhub.view.layouts.SpacedColumn
 
 @Composable
 fun FileInput() {
@@ -44,14 +60,17 @@ fun FileInput() {
         }
     )
 
-    Column {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(5.dp))
-        ) {
+    var buttonLabelFile by remember {
+        mutableStateOf("")
+    }
+
+    SpacedColumn(
+        spacing = 8,
+        verticalAlignment = Alignment.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(top = 8.dp)
+    ) {
+        Row {
             Button(
                 onClick = {
                     photoPickerLauncher.launch(
@@ -60,22 +79,47 @@ fun FileInput() {
                 },
                 colors = ButtonDefaults.buttonColors(),
                 shape = RoundedCornerShape(5.dp),
-                modifier = Modifier
-                    .border(
-                        width = 1.dp,
-                        color = Color.Gray,
-                        shape = RoundedCornerShape(5.dp),
-                    )
-                    .fillMaxWidth()
+                modifier = Modifier.weight(1f)
             ) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.upload),
                     contentDescription = null,
                     modifier = Modifier.padding(horizontal = 8.dp, vertical = 3.dp)
                 )
-                Text(text = "Adicionar anexo")
+                Text(text = buttonLabelFile)
             }
+
+            if (selectImageUri != null) {
+                buttonLabelFile = "Editar anexo"
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Button(
+                    onClick = {
+                        selectImageUri = null
+                        buttonLabelFile = "Adicionar anexo"
+                    },
+                    shape = RoundedCornerShape(5.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Delete,
+                        contentDescription = null,
+                        modifier = Modifier.padding(vertical = 3.dp)
+                    )
+                }
+            } else {
+                buttonLabelFile = "Adicionar anexo"
+            }
+
         }
+
+        AsyncImage(
+            model = selectImageUri,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(240.dp)
+        )
     }
 }
 
@@ -83,8 +127,6 @@ fun FileInput() {
 @Composable
 fun FileInputPreview() {
     MindHubTheme {
-        Surface(modifier = Modifier.padding(16.dp)) {
-            FileInput()
-        }
+        FileInput()
     }
 }
