f264a3b4a3b45285321bd900c7795c6901edbdd6
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-06 15:31:36 -0300

    feat: add date formatter function

--- app/src/main/java/com/mindhub/common/ext/DateTime.kt
+++ app/src/main/java/com/mindhub/common/ext/DateTime.kt
@@ -3,6 +3,8 @@ package com.mindhub.common.ext
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TimePickerState
 import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
@@ -22,4 +24,9 @@ fun Long.toBrazilianDateFormat(
 @OptIn(ExperimentalMaterial3Api::class)
 fun TimePickerState.toTimeFormat(): String {
     return "${this.hour.toString().padStart(2, '0')}:${this.minute.toString().padStart(2, '0')}"
+}
+
+fun LocalDateTime.toBrazilianDateFormat(): String {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    return this.format(formatter)
 }
