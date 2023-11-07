97fd65778f795ebf5963c00c405ee2e16f1067f6
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-07 12:49:31 -0300

    feat: add view model logic in file input composable with bug editing logic

--- app/src/main/java/com/mindhub/view/composables/FileInput.kt
+++ app/src/main/java/com/mindhub/view/composables/FileInput.kt
@@ -1,19 +1,10 @@
 package com.mindhub.view.composables
 
-import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,9 +15,6 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -36,27 +24,30 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.mindhub.R
+import com.mindhub.model.entities.Expertise
 import com.mindhub.ui.theme.MindHubTheme
 import com.mindhub.view.layouts.SpacedColumn
+import com.mindhub.viewmodel.ask.AskViewModel
 
 @Composable
-fun FileInput() {
-    var selectImageUri by remember {
-        mutableStateOf<Uri?>(null)
-    }
+fun FileInput(
+    askViewModel: AskViewModel
+) {
+//    var selectImageUri by remember {
+//        mutableStateOf<Uri?>(askViewModel.file)
+//    }
 
     val photoPickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia(),
         onResult = {
-            selectImageUri = it
+//            selectImageUri = it
+            askViewModel.file = it
         }
     )
 
@@ -89,14 +80,15 @@ fun FileInput() {
                 Text(text = buttonLabelFile)
             }
 
-            if (selectImageUri != null) {
+            if (askViewModel.file != null) {
                 buttonLabelFile = "Editar anexo"
 
                 Spacer(modifier = Modifier.width(8.dp))
 
                 Button(
                     onClick = {
-                        selectImageUri = null
+//                        selectImageUri = null
+                        askViewModel.file = null
                         buttonLabelFile = "Adicionar anexo"
                     },
                     shape = RoundedCornerShape(5.dp),
@@ -110,23 +102,31 @@ fun FileInput() {
             } else {
                 buttonLabelFile = "Adicionar anexo"
             }
-
         }
 
-        AsyncImage(
-            model = selectImageUri,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(240.dp)
-        )
+        if (askViewModel.file != null) {
+            AsyncImage(
+                model = askViewModel.file,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .clip(RoundedCornerShape(5))
+            )
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun FileInputPreview() {
+    val ask = AskViewModel()
+
+    ask.title = "Como somar suas raízes"
+    ask.content = "Não sei como somar duas raízes quadradas, alguém pode me ajudar?"
+    ask.expertise = Expertise("Matemática")
+
     MindHubTheme {
-        FileInput()
+        FileInput(ask)
     }
 }
