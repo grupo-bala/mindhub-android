0a0de21346e30159d6b0e9ce9a16014f65b2abdd
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-07 12:43:31 -0300

    feat: add file attribute in ask entity

--- app/src/main/java/com/mindhub/model/entities/Ask.kt
+++ app/src/main/java/com/mindhub/model/entities/Ask.kt
@@ -1,6 +1,8 @@
 package com.mindhub.model.entities
 
+import android.net.Uri
 import com.mindhub.common.serialize.DateSerializer
+import com.mindhub.common.serialize.UriSerializer
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 
@@ -14,4 +16,6 @@ data class Ask(
     @Serializable(DateSerializer::class)
     override var postDate: LocalDateTime,
     var expertise: Expertise,
+    @Serializable(UriSerializer::class)
+    var file: Uri? = null
 ) : Post
