d3611239b1d6c09be89e231bc80b0b0f9f15fbec
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-12 18:16:47 -0300

    feat: add serializable annotations in event entity

--- app/src/main/java/com/mindhub/model/entities/Event.kt
+++ app/src/main/java/com/mindhub/model/entities/Event.kt
@@ -1,14 +1,19 @@
 package com.mindhub.model.entities
 
+import com.mindhub.common.serialize.DateSerializer
+import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 
+@Serializable
 data class Event(
     override var id: Int,
     override var user: User,
     override var title: String,
     override var content: String,
     override var score: Int,
+    @Serializable(DateSerializer::class)
     override var postDate: LocalDateTime,
+    @Serializable(DateSerializer::class)
     var date: LocalDateTime,
     var latitude: Double,
     var longitude: Double,
