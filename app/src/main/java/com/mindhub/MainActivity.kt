c37674985f1fb2024a4783ca2563198bcceddcf8
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-18 16:43:51 -0300

    fix: fix keyboard white background issues

--- app/src/main/java/com/mindhub/MainActivity.kt
+++ app/src/main/java/com/mindhub/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mindhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.mindhub.ui.theme.MindHubTheme
 import com.mindhub.view.NavGraphs
 import com.ramcosta.composedestinations.DestinationsNavHost
@@ -10,6 +12,10 @@ import com.ramcosta.composedestinations.DestinationsNavHost
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val transparentColor = Color.Transparent.toArgb()
+        window.decorView.rootView.setBackgroundColor(transparentColor)
+
         setContent {
             MindHubTheme {
                 DestinationsNavHost(navGraph = NavGraphs.root)
