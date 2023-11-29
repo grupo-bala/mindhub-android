dadeb95093d3b5f0dba4493f43fed34af332e3b0
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-11-29 19:14:14 -0300

    feat: update error parser with comment errors

--- app/src/main/java/com/mindhub/common/services/ErrorParser.kt
+++ app/src/main/java/com/mindhub/common/services/ErrorParser.kt
@@ -8,6 +8,9 @@ object ErrorParser {
             "DUPLICATE USERNAME" -> "Esse nome de usuário já está em uso"
             "USER NEED AT LEAST ONE EXPERTISE" -> "Selecione pelo menos 1 área de especialidade"
             "USER CAN HAVE UNTIL 3 EXPERTISES" -> "Você pode ter no máximo 3 áreas de especialidade"
+            "POST DOESNT EXIST" -> "O post não existe"
+            "COMMENT DOESNT EXIST" -> "O comentário não existe"
+            "COMMENT TO REPLY OR POST DOESNT EXIST" -> "O comentário ou o post não existe"
             else -> "Ops... Houve um erro inesperado"
         }
     }
