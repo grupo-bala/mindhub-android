fccf30fc3b372124e197fc2400b85457fd4d885d
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-29 06:01:29 -0300

    chore: update coil dependecy version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,5 +92,6 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
 }
