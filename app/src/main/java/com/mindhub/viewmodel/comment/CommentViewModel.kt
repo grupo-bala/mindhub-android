cf9b6b3e050cd86e72e3e3a1d1898a3e7478e2da
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-11-29 19:16:08 -0300

    feat: update comment related view models

---
+++ app/src/main/java/com/mindhub/viewmodel/comment/CommentViewModel.kt
@@ -0,0 +1,208 @@
+package com.mindhub.viewmodel.comment
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mindhub.model.entities.Comment
+import com.mindhub.common.services.ErrorParser
+import com.mindhub.model.api.CommentApi
+import com.mindhub.model.api.CreateCommentRequest
+import com.mindhub.model.api.CreateReplyRequest
+import com.mindhub.model.api.ScoreApi
+import com.mindhub.model.api.ScoreCommentRequest
+import com.mindhub.model.api.UpdateCommentRequest
+import kotlinx.coroutines.launch
+
+class CommentViewModel : ViewModel() {
+    var comments = mutableStateListOf<Comment>()
+    var feedback by mutableStateOf("")
+    var isLoading by mutableStateOf(false)
+    var update by mutableStateOf(false)
+
+    fun get(postId: Int, onFailure: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                feedback = ""
+                isLoading = true
+
+                comments.clear()
+                comments.addAll(CommentApi.findAll(postId))
+
+                comments.sortedWith(
+                    compareByDescending<Comment> { it.isBestAnswer }.thenByDescending { it.score }
+                )
+            } catch (e: Exception) {
+                feedback = ErrorParser.from(e.message)
+
+                onFailure()
+            }
+
+            isLoading = false
+        }
+    }
+
+    fun addComment(postId: Int, content: String, onFailure: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                feedback = ""
+
+                val addedComment =
+                    CommentApi.create(CreateCommentRequest(content, postId))
+
+                comments.add(addedComment)
+
+                comments.sortedWith(
+                    compareByDescending<Comment> { it.isBestAnswer }.thenByDescending { it.score }
+                )
+            } catch (e: Exception) {
+                feedback = ErrorParser.from(e.message)
+
+                onFailure()
+            }
+        }
+    }
+
+    fun addReply(postId: Int, commentId: Int, content: String, onFailure: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                feedback = ""
+
+                val addedReply = CommentApi.createReply(CreateReplyRequest(content, postId, commentId))
+
+                val targetComment = comments.find { it.id == commentId } ?: throw Exception()
+                val index = comments.indexOf(targetComment)
+
+                comments.remove(targetComment)
+
+                targetComment.replies.add(addedReply)
+
+                comments.add(index, targetComment)
+            } catch (e: Exception) {
+                feedback = ErrorParser.from(e.message)
+
+                onFailure()
+            }
+        }
+    }
+
+    fun updateScore(commentId: Int, userScore: Int, onFailure: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                feedback = ""
+
+                val targetComment = comments.find { it.id == commentId } ?: throw Exception()
+                val index = comments.indexOf(targetComment)
+
+                if (userScore == targetComment.userScore) {
+                    targetComment.score += targetComment.userScore * -1
+                    targetComment.userScore = 0
+                } else {
+                    targetComment.score += targetComment.userScore * -1
+                    targetComment.score += if (targetComment.userScore == 0) userScore else 0
+                    targetComment.userScore = if (targetComment.userScore == 0) userScore else 0
+                }
+
+                ScoreApi.vote(ScoreCommentRequest(commentId, targetComment.userScore))
+
+                comments.removeAt(index)
+                comments.add(index, targetComment)
+
+                comments.sortedWith(
+                    compareByDescending<Comment> { it.isBestAnswer }.thenByDescending { it.score }
+                )
+            } catch (e: Exception) {
+                feedback = ErrorParser.from(e.message)
+
+                onFailure()
+            }
+        }
+    }
+
+    fun removeComment(commentId: Int, replyTo: Int?, onFailure: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                feedback = ""
+
+                CommentApi.remove(commentId)
+
+                if (replyTo != null) {
+                    val targetComment = comments.find { it.id == replyTo } ?: throw Exception()
+                    val index = comments.indexOf(targetComment)
+
+                    comments.remove(targetComment)
+
+                    targetComment.replies.removeIf { it.id == commentId }
+
+                    comments.add(index, targetComment)
+                } else {
+                    comments.removeIf { it.id == commentId }
+                }
+            } catch (e: Exception) {
+                feedback = ErrorParser.from(e.message)
+
+                onFailure()
+            }
+        }
+    }
+
+    fun updateComment(commentId: Int, replyTo: Int?, newContent: String, onFailure: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                feedback = ""
+
+                CommentApi.update(commentId, UpdateCommentRequest(newContent))
+
+                val targetComment = comments.find { it.id == (replyTo ?: commentId) } ?: throw Exception()
+                val index = comments.indexOf(targetComment)
+
+                comments.remove(targetComment)
+
+                if (replyTo != null) {
+                    val targetReply = targetComment.replies.find { it.id == commentId } ?: throw Exception()
+
+                    targetReply.content = newContent
+                } else {
+                    targetComment.content = newContent
+                }
+
+                comments.add(index, targetComment)
+            } catch (e: Exception) {
+                feedback = ErrorParser.from(e.message)
+
+                onFailure()
+            }
+        }
+    }
+    
+    fun toggleBestAnswer(commentId: Int, postId: Int, onFailure: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                feedback = ""
+
+                CommentApi.toggleBestAnswer(commentId, postId)
+
+                val oldBestAnswer = comments.find { it.isBestAnswer }
+
+                if (oldBestAnswer != null) {
+                    oldBestAnswer.isBestAnswer = false
+                }
+
+                val targetComment = comments.find { it.id == commentId } ?: throw Exception()
+                val index = comments.indexOf(targetComment)
+
+                comments.remove(targetComment)
+
+                targetComment.isBestAnswer = true
+
+                comments.add(index, targetComment)
+            } catch (e: Exception) {
+                feedback = ErrorParser.from(e.message)
+
+                onFailure()
+            }
+        }
+    }
+}
--- app/src/main/java/com/mindhub/viewmodel/post/GetPostViewModel.kt
+++ app/src/main/java/com/mindhub/viewmodel/post/GetPostViewModel.kt
@@ -3,7 +3,7 @@ package com.mindhub.viewmodel.post
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mindhub.model.api.ScoreApi
-import com.mindhub.model.api.ScoreRequest
+import com.mindhub.model.api.ScorePostRequest
 import com.mindhub.model.entities.Post
 import kotlinx.coroutines.launch
 
@@ -18,14 +18,15 @@ abstract class GetPostViewModel : ViewModel() {
         viewModelScope.launch {
             if (userScore == post!!.userScore) {
                 post!!.score += post!!.userScore * -1
-                ScoreApi.vote(ScoreRequest(post!!.id, 0))
+                post!!.userScore = 0
+
             } else {
                 post!!.score += post!!.userScore * -1
-                post!!.userScore = userScore
-                post!!.score += userScore
-
-                ScoreApi.vote(ScoreRequest(post!!.id, userScore))
+                post!!.score += if (post!!.userScore == 0) userScore else 0
+                post!!.userScore = if (post!!.userScore == 0) userScore else 0
             }
+
+            ScoreApi.vote(ScorePostRequest(post!!.id, userScore))
         }
 
         post = post
