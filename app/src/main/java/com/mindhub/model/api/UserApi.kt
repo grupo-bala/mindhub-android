b07b550a304201757d4597641a9bbed78b58e12b
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-11-29 23:05:16 -0300

    feat: add update method in user api

--- app/src/main/java/com/mindhub/model/api/UserApi.kt
+++ app/src/main/java/com/mindhub/model/api/UserApi.kt
@@ -1,11 +1,7 @@
 package com.mindhub.model.api
 
-import android.net.Uri
 import com.mindhub.BuildConfig
-import com.mindhub.common.serialize.UriSerializer
-import com.mindhub.model.entities.Badge
 import com.mindhub.model.entities.Expertise
-import com.mindhub.model.entities.User
 import com.mindhub.common.services.CurrentUser
 import com.mindhub.common.services.UserInfo
 import io.ktor.client.call.body
@@ -35,19 +31,17 @@ data class RegisterRequest(
 )
 
 @Serializable
-data class UpdateRequest(
+data class UpdateInfoRequest(
     val name: String,
     val email: String,
+    val password: String,
     val expertises: List<Expertise>,
-    val badge: Badge,
-    @Serializable(UriSerializer::class)
-    val profilePicture: Uri?,
 )
 
 interface UserProvider {
     suspend fun login(params: LoginRequest)
     suspend fun register(params: RegisterRequest)
-    suspend fun update(params: UpdateRequest)
+    suspend fun update(params: UpdateInfoRequest, profilePicture: ByteArray?)
 }
 
 object UserApi : UserProvider {
@@ -83,72 +77,22 @@ object UserApi : UserProvider {
         CurrentUser.token = userInfo.token
     }
 
-    override suspend fun update(params: UpdateRequest) {
-        Api.patch("${BuildConfig.apiPrefix}/user/${CurrentUser.user!!.username}") {
+    override suspend fun update(params: UpdateInfoRequest, profilePicture: ByteArray?) {
+        val response: HttpResponse = Api.patch("${BuildConfig.apiPrefix}/user/${CurrentUser.user!!.username}") {
             contentType(ContentType.Application.Json)
             headers {
                 append("Authorization", "Bearer ${CurrentUser.token}")
             }
             setBody(params)
         }
-    }
-}
 
-object UserFakeApi : UserProvider {
-    private val users = mutableListOf<User>().also {
-        it.add(
-            User(
-                name = "Administrador",
-                email = "admin@admin.com",
-                username = "admin",
-                xp = 0,
-                currentBadge = Badge("Aprendiz", 0),
-                badges = listOf(),
-                expertises = listOf(
-                    Expertise("Matemática"),
-                    Expertise("Física"),
-                    Expertise("Inglês")
-                )
-            )
-        )
-    }
-
-    override suspend fun login(params: LoginRequest) {
-        CurrentUser.user = users.find { it.email == params.email && params.password == "123" }
-            ?: throw Exception("WRONG CREDENTIALS")
-    }
-
-    override suspend fun register(params: RegisterRequest) {
-        if (users.find { it.username == params.username } != null) {
-            throw Exception("DUPLICATE USERNAME")
-        } else if (users.find { it.email == params.email } != null) {
-            throw Exception("DUPLICATE EMAIL")
+        if (response.status != HttpStatusCode.OK) {
+            println(response.body<ApiError>().message)
+            throw Exception(response.body<ApiError>().message)
         }
 
-        val user = User(
-            name = params.name,
-            email = params.email,
-            username = params.username,
-            xp = 0,
-            currentBadge = Badge("Aprendiz", 0),
-            badges = listOf(),
-            expertises = params.expertises.map { Expertise(it) },
-        )
-
-        users.add(user)
-        CurrentUser.user = user
-    }
-
-    override suspend fun update(params: UpdateRequest) {
-        val user = users.find { it.username == CurrentUser.user!!.username }
-            ?: throw Exception()
-
-        user.name = params.name
-        user.email = params.email
-        user.expertises = params.expertises
-        user.currentBadge = params.badge
-        user.profilePicture = params.profilePicture
-
-        CurrentUser.user = user
+        if (profilePicture != null) {
+            FileApi.uploadUser(CurrentUser.user!!.username, profilePicture)
+        }
     }
 }
