06e8e9d2f3591fcfc6540e49eeb8bc5bc7867d3f
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-29 21:54:26 -0300

    feat: add lifecycle runtime dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,5 +93,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("io.coil-kt:coil-compose:2.5.0")
-
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-beta01")
 }
