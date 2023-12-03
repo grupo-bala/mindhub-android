bbddc63867ce953540bb6e1b2fb0010acab331a3
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-12-03 19:43:59 -0300

    feat: update badge view model

--- app/src/main/java/com/mindhub/viewmodel/badge/BadgeViewModel.kt
+++ app/src/main/java/com/mindhub/viewmodel/badge/BadgeViewModel.kt
@@ -5,35 +5,12 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.mindhub.model.api.BadgeFakeApi
 import com.mindhub.model.entities.Badge
 import com.mindhub.common.services.CurrentUser
-import kotlinx.coroutines.launch
 
-class BadgeViewModel() : ViewModel() {
+class BadgeViewModel : ViewModel() {
     var selectedBadge by mutableStateOf(CurrentUser.user!!.currentBadge)
-    var unlockedBadges = mutableStateListOf<Badge>()
-
-    private var isBadgesLoaded = false
-
-    fun loadBadges() {
-        if (!isBadgesLoaded) {
-            viewModelScope.launch {
-                try {
-                    unlockedBadges.addAll(BadgeFakeApi.getUnlockedBadges(CurrentUser.user!!.xp))
-                } catch (_: Exception) {}
-            }
-
-            isBadgesLoaded = true
-        }
-    }
-
-    fun selectBadge(badge: Badge) {
-        if (!unlockedBadges.contains(badge)) {
-            throw Exception()
-        }
-
-        selectedBadge = badge
+    var unlockedBadges = mutableStateListOf<Badge>().also {
+        it.addAll(CurrentUser.user!!.badges)
     }
 }
